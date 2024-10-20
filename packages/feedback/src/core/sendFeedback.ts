afa6d817e010c5a00662e22529b79aa2ae5aa61e
Joe Malatesta <106361624+joemmalatesta@users.noreply.github.com>
2024-10-20 00:49:45 -0700

    Update sendFeedback.ts

--- packages/feedback/src/core/sendFeedback.ts
+++ packages/feedback/src/core/sendFeedback.ts
@@ -4,7 +4,7 @@ import { getCurrentScope } from '@sentry/core';
 import type { Event, EventHint, SendFeedback, SendFeedbackParams, TransportMakeRequestResponse } from '@sentry/types';
 import { getLocationHref } from '@sentry/utils';
 import { FEEDBACK_API_SOURCE } from '../constants';
-//Import deez nuts
+//Import deez nuts I got motion
 
 /**
  * Public API to send a Feedback item to Sentry
