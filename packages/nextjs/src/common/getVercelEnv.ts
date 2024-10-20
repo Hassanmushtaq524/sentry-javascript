319519f417d45b05cee4d31f76b3063d4c9fdb8c
Joe Malatesta <106361624+joemmalatesta@users.noreply.github.com>
2024-10-20 00:17:38 -0700

    Update getVercelEnv.ts

--- packages/nextjs/src/common/getVercelEnv.ts
+++ packages/nextjs/src/common/getVercelEnv.ts
@@ -7,3 +7,5 @@ export function getVercelEnv(isClient: boolean): string | undefined {
   const vercelEnvVar = isClient ? process.env.NEXT_PUBLIC_VERCEL_ENV : process.env.VERCEL_ENV;
   return vercelEnvVar ? `vercel-${vercelEnvVar}` : undefined;
 }
+
+// We're just balling.
